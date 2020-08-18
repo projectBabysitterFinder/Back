0f4c64ae57140e449d7ff15ddb34366a96e35ef9
hadiazb <hadiazb@unal.edu.co>
2020-08-18 17:09:40 -0500

    [ADD] babysitter service

---
+++ src/api/components/babysitter/controller.js
@@ -0,0 +1,57 @@
+// const auth = require('../auth');
+// const { nanoid } = require('nanoid');
+const TABLA = 'babysitter';
+
+module.exports = (injectedStore) => {
+	let store = injectedStore;
+
+	if (!store) {
+		store = require('../../../store/dummy');
+	}
+
+	const list = () => {
+		return store.list(TABLA);
+	};
+
+	const get = (id) => {
+		return store.get(TABLA, id);
+	};
+
+	// const upsert = async (body) => {
+	// 	const user = {
+	// 		id: body.id,
+	// 		name: body.name,
+	// 		username: body.username,
+	// 		email: body.email,
+	// 		phone: body.phone,
+	// 		create: body.create,
+	// 		status: body.status,
+	// 	};
+
+	// 	if (body.id) {
+	// 		user.id = body.id;
+	// 	} else {
+	// 		user.id = nanoid();
+	// 	}
+
+	// 	if (body.password || body.username) {
+	// 		await auth.upsert({
+	// 			id: user.id,
+	// 			username: user.username,
+	// 			password: body.password,
+	// 		});
+	// 	}
+
+	// 	return store.upsert(TABLA, user);
+	// };
+
+	const remove = async (id) => {
+		return await store.remove(TABLA, id);
+	};
+
+	return { 
+    list, 
+    get, 
+    // upsert, 
+    remove };
+};
--- src/store/db.js
+++ src/store/db.js
@@ -27,10 +27,58 @@ module.exports = {
 
 			admin_status: false,
 		},
+				{
+			id: 'sdsaevaskds',
+			name: 'Carla Giraldo',
+			username: 'https://google.com',
+			photo: '00000000',
+			password: 'carlita123',
+			age: '26',
+			phone: '123454567',
+			email: 'carlita123@gmail.com',
+
+			country: 'Colombia',
+			state: 'Bogota D.C.',
+			city: 'Bogota',
+			address: 'Carrera 30 # 45-13',
+
+			create: '2019-03-25 09:53:05.78888851 +05:30',
+			update: '2019-03-31 09:53:05.78888851 +05:30',
+
+			availability: 'Todo el tiempo soy una desocupada ',
+			specialty: 'Ardiente',
+			experience: '4 años',
+
+			services: 5,
+			reviews: 3,
+
+			admin_status: false,
+		},
 	],
 	user: [
 		{
 			id: 'aseferfea',
+			name: 'Mario',
+			username: 'mariucho',
+			password: 'carlita123',
+			phone: '123454567',
+			email: 'carlita123@gmail.com',
+
+			country: 'Colombia',
+			state: 'Bogota D.C.',
+			city: 'Bogota',
+			address: 'Carrera 30 # 45-13',
+
+			create: '2019-03-25 09:53:05.78888851 +05:30',
+			update: '2019-03-31 09:53:05.78888851 +05:30',
+
+			admin_status: false,
+
+			request_services: 5,
+			score: 3,
+		},
+		{
+			id: 'aseferea',
 			name: 'Mario',
 			username: 'mariucho',
 			password: 'carlita123',
