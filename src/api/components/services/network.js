717d088b7d87c5c54ba82f15cdcdd770048082bd
hadiazb <hadiazb@unal.edu.co>
2020-08-18 17:09:58 -0500

    [ADD] babysitter service

---
+++ src/api/components/services/network.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const response = require('../../../network/response');
+const router = express.Router();
+const controller = require('./index');
+
+const list = (req, res, next) => {
+	controller
+		.list()
+		.then((list) => {
+			response.success(req, res, list, 200);
+		})
+		.catch(next);
+};
+
+const get = (req, res, next) => {
+	controller
+		.get(req.params.id)
+		.then((user) => {
+			response.success(req, res, user, 200);
+		})
+		.catch(next);
+};
+
+// const upsert = (req, res, next) => {
+// 	controller
+// 		.upsert(req.body)
+// 		.then((user) => {
+// 			response.success(req, res, user, 201);
+// 		})
+// 		.catch(next);
+// };
+
+const remove = (req, res, next) => {
+	controller
+		.remove(req.params.id)
+		.then((report) => {
+			response.success(req, res, report, 201);
+		})
+		.catch(next);
+};
+
+router.get('/', list);
+router.get('/:id', get);
+// router.post('/', upsert);
+// router.put('/', upsert);
+// router.delete('/:id', remove);
+
+module.exports = router;
