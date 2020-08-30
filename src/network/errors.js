81f8ac79371c726846ea517b84ad0af8d246632b
daff <daffmg1@gmail.com>
2020-08-30 09:08:38 -0500

    log integrado para el stack full de los errores

--- src/network/errors.js
+++ src/network/errors.js
@@ -1,8 +1,10 @@
 const response = require('./response');
+var fs = require('fs'); 
+var log_file = fs.createWriteStream(__dirname + '/node.log', {flags : 'a'});
 
 const errors = (err, req, res, next) => {
 	console.error('[error]', err);
-
+	log_file.write(err.stack)
 	const message = err.message || 'Error interno';
 	const status = err.statusCode || 500;
 
