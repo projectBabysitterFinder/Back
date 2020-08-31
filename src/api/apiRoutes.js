1ba0e973c14f99f642a39e46addcbaf56864b9ec
hadiazb <hadiazb@unal.edu.co>
2020-08-30 21:42:06 -0500

    Deploy

--- src/api/apiRoutes.js
+++ src/api/apiRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const response = require('../network/response');
 const router = express.Router();
+require('dotenv').config();
 
 router.get('/', (req, res) => {
   response.success(req, res, message, 200);
@@ -10,8 +11,8 @@ module.exports = router;
 
 const message = [
   {
-    user: 'http://localhost:3000/api/users',
-    reviews: 'http://localhost:3000/api/reviews',
-    services: 'http://localhost:3000/api/services',
+    user: `http://localhost:${process.env.PORT}/api/users`,
+    reviews: `http://localhost:${process.env.PORT}/api/reviews`,
+    services: `http://localhost:${process.env.PORT}/api/services`,
   },
 ];
